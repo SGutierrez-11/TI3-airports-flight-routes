279cc04223b6adf155c7626f021ce2b22819aa4d
Martin Perez Lopez <martin.perez2@correo.icesi.edu.co>
2021-11-30 19:57:56 -0500

    Create GUI

---
+++ src/ui/MainGUI.java
@@ -0,0 +1,42 @@
+package ui;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+public class MainGUI {
+    @FXML
+    private Pane mainPane;
+    @FXML
+    private TextField txtOrigin;
+    @FXML
+    private TextField txtDestination;
+    @FXML
+    private TextArea txtResult;
+    @FXML
+    void backMain(ActionEvent event) {
+    	FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("area-main.fxml"));
+    	fxmlLoader.setController(this);
+		Parent userView;
+		try {
+			userView = fxmlLoader.load();
+			mainPane.getChildren().clear();
+			mainPane.getChildren().add(userView);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    @FXML
+    void calculateFlight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void calculateRoundTrip(ActionEvent event) {
+
+    }
+}
